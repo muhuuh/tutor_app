8c7262577753e614987894531abf27156989f5f9
arani <aranimontes@protonmail.com>
2025-03-16 07:45:48 +0100

    fix deploy error

--- src/pages/SocialMedia/SocialMediaVisual.tsx
+++ src/pages/SocialMedia/SocialMediaVisual.tsx
@@ -3,7 +3,6 @@ import { motion } from "framer-motion";
 import { AuroraBackground } from "../../components/UI/aurora-background";
 import { SocialAuroraBackground } from "../../components/UI/SocialAuroraBackground";
 import {
-  FiCheck,
   FiActivity,
   FiVideo,
   FiEdit,
